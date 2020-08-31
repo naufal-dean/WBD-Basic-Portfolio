bc3c6dcafdc6bbf38274e23a1e62f87d8856746b
naufal-dean <naufal.dean@gmail.com>
2020-08-31 14:06:09 +0700

    Add personal data in view animation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 window.onload = function() {
+  // Handler definition
+  const personalDataScrollHandler = function(event) {
+    const personalDataSection = document.querySelector("#personal-data");
+    const personalDataSectionL = document.querySelector("#personal-data div.content-left>div");
+    const personalDataSectionR = document.querySelector("#personal-data div.content-right>div");
 
+    if (personalDataSection.getBoundingClientRect().top <= 200) {
+      personalDataSectionL.classList.remove("hidden");
+      personalDataSectionR.classList.remove("hidden");
+    } else if (window.scrollY <= 50) {
+      personalDataSectionL.classList.add("hidden");
+      personalDataSectionR.classList.add("hidden");
+    }
+  };
+  // Initial load call
+  personalDataScrollHandler();
+
+  // Register listener
+  window.addEventListener("scroll", personalDataScrollHandler);
 };
